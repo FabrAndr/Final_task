ccc072bf11c32935c34037d1197811d6a11dbed9
Fabr <fabrics.andrey@yandex.ru>
2024-01-14 16:55:38 +0300

    New version - i have fixed the problem with empty spaces

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,35 +1,44 @@
 ﻿
+void showstring(string [] str){ //function to show the massive
+     Console.Write($" the new massive is [{str[1]}");
+     for (int i = 1; i < str.Length - 1; i++){
+       if (str[i] == null) ;
+       else Console.Write($", {str[i]}");
+        }
+    if (str[str.Length -1] != null) Console.Write($", {str[str.Length - 1]} ];");    // I could fix the problem with empty spaces,
+    else Console.Write("]");                                                       // but it's fiction, empty spaces are 
+    }                                                                              //still into the massive
+
+string[]  sort(string[] massive){ //function of sorting
+    string[] stock = new string[massive.Length];
+    int line = 0;
+    for (int i = 0; i < massive.Length; i++){
+        if (massive[i].Length >= 3) {
+           
+           stock[line] = massive[i];
+           line++;
+        }
+    }
+    return stock;
+}
+
 Console.WriteLine("Введите количество символов");
-int N = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());     //create a massive
 string[] column = new string[N];
 for (int i = 0; i < N; i++){
      column[i] = Console.ReadLine();
 }
 
-string[]  sort(string[] massive){
-    string[] stock = new string[massive.Length];
-    int line = 0;
-    for (int i = 0; i < massive.Length; i++){
-        if (massive[i].Length >= 3) {
-           stock[line] = massive[i];
-           line++;
-        }
-        else ;
-    }
-    return stock;
 
-}
 string[] st = sort(column);
 
-void showstring(string [] str){
-     Console.Write(" the new massive is [");
-     for (int i = 0; i < str.Length - 1; i++){
-        Console.Write($" {str[i]},");
-        }
-    Console.Write($"{str[str.Length - 1]} ];");
-    }
-
 showstring(st);
+
+//we need smt to delete empty spaces
+
+
+
+
 
 // Console.Write("M = ");
 // int M = int.Parse(Console.ReadLine());
